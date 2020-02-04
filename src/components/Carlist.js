991b5f3eb8991c60181aef38faa3951b674464fd
Jani <jani@xps>
2020-02-04 23:00:43 +0200

    Implement delete car

--- src/components/Carlist.js
+++ src/components/Carlist.js
@@ -9,8 +9,15 @@ export default function Carlist() {
 
   const fetchData = () => {
     fetch('https://carstockrest.herokuapp.com/cars')
-      .then(response => response.json())
-      .then(data => setCars(data._embedded.cars))
+    .then(response => response.json())
+    .then(data => setCars(data._embedded.cars))
+    .catch(err => console.error(err))
+  }
+
+  const deleteCar = (link) => {
+    fetch(link, {method: 'DELETE'})
+    .then(response => fetchData())
+    .catch(err => console.error(err))
   }
 
   const columns = [
@@ -42,6 +49,10 @@ export default function Carlist() {
       Header: 'Price',
       accessor: 'price',
     },
+    {
+      accessor: '_links.self.href',
+      Cell: row => <button onClick={() => deleteCar(row.value)}>Delete</button>
+    }
   ]
 
   return (
